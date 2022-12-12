8753be1831565347158c2eaadb814861ad1ef188
Kai W <simaow@outlook.com>
2022-12-12 11:26:38 +1100

    add loading

--- app/Article.tsx
+++ app/Article.tsx
@@ -1,3 +1,4 @@
+import LiveTime from "./LiveTime"
 import ReadMoreButton from "./ReadMoreButton"
 
 type Props = {
@@ -23,7 +24,7 @@ function Article({ article }: Props) {
             </section>
             <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
                 <p>{article.source}</p>
-                <p>{article.published_at}</p>
+                <p><LiveTime time={article.published_at} /></p>
             </footer>
         </div>
 
